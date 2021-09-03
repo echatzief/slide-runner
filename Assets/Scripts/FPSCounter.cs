f31d3ba5e9b51008cbc5c24543c884e1b597d323
unknown <echatzief97@gmail.com>
2021-09-03 12:02:56 +0300

    Added the car as obstacle. Lifes before dead

--- Assets/Scripts/FPSCounter.cs
+++ Assets/Scripts/FPSCounter.cs
@@ -5,23 +5,17 @@ public class FPSCounter : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    private Text fpstext;
+    private Text lifes;
     private float HUDRefreshRate = 1f;
     private float timer;
 
     void Start()
     {
-        fpstext = gameObject.GetComponent<Text>();
+       lifes.text = "10";
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Time.unscaledTime > timer)
-        {
-            int fps = (int)(1f / Time.unscaledDeltaTime);
-            fpstext.text = fps.ToString();
-            timer = Time.unscaledTime + HUDRefreshRate;
-        }
     }
 }
--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -1,16 +1,21 @@
 ﻿using UnityEngine;
-
+using UnityEngine.UI;
 public class PlayerCollision : MonoBehaviour
 {
 
     public PlayerMovement movement;
-
+    public GameObject lifes;
     void OnCollisionEnter(Collision collisionInfo)
     {
-        if (collisionInfo.collider.tag == "Obstacle")
-        {
-            movement.enabled = false;
-            FindObjectOfType<GameManager>().EndGame();
+        Debug.Log(collisionInfo.collider.tag);
+        if (collisionInfo.collider.tag == "Obstacle"){
+            Text lifesText = lifes.GetComponent<Text>();
+            if(int.Parse(lifesText.text) > 0){
+                lifesText.text = (int.Parse(lifesText.text) - 1 ).ToString();
+            } else {
+                movement.enabled = false;
+                PauseMenu.showScoreCard();
+            }
         }
     }
 }
